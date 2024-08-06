b0301f58ce8a1ba6f9a135ab72e1a3b1e478e830
Rafael Coelho <rafaelcoelhofullstack@gmail.com>
2024-08-06 00:18:36 -0300

    Ajustando verificação de usuário criado - PHP Crud - ACTO

--- acoes.php
+++ acoes.php
@@ -2,13 +2,13 @@
 session_start();
 require 'conexao.php';
 
-if(isset($_POST['create_usuario'])){
+if (isset($_POST['create_usuario'])){
     $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
     $email = mysqli_real_escape_string($conexao, trim($_POST['email']));
     $data_nascimento = mysqli_real_escape_string($conexao, trim($_POST['data_nascimento']));
     $senha = isset($_POST['senha']) ? mysqli_real_escape_string($conexao, password_hash(trim($_POST['nome']), PASSWORD_DEFAULT)) : '';
 
-    $sql = "INSERT INTO usuarios (nome, email, data_nascimento, senha) VALUES('$nome', '$email', '$data_nascimento', '$senha')";
+    $sql = "INSERT INTO usuarios (nome, email, data_nascimento, senha) VALUES ('$nome', '$email', '$data_nascimento', '$senha')";
 
     mysqli_query($conexao, $sql);
 

68b4b3cd8447f2823ae4e81177c25093ff213b42
Rafael Coelho <rafaelcoelhofullstack@gmail.com>
2024-08-07 15:29:51 -0300

    ajustando páginas em PHP

--- acoes.php
+++ acoes.php
@@ -1,27 +1,26 @@
 <?php
-session_start();
-require 'conexao.php';
+    session_start();
+    require 'conexao.php';
 
-if (isset($_POST['create_usuario'])){
-    $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
-    $email = mysqli_real_escape_string($conexao, trim($_POST['email']));
-    $data_nascimento = mysqli_real_escape_string($conexao, trim($_POST['data_nascimento']));
-    $senha = isset($_POST['senha']) ? mysqli_real_escape_string($conexao, password_hash(trim($_POST['nome']), PASSWORD_DEFAULT)) : '';
+    if (isset($_POST['create_usuario'])){
+        $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
+        $email = mysqli_real_escape_string($conexao, trim($_POST['email']));
+        $data_nascimento = mysqli_real_escape_string($conexao, trim($_POST['data_nascimento']));
+        $senha = isset($_POST['senha']) ? mysqli_real_escape_string($conexao, password_hash(trim($_POST['nome']), PASSWORD_DEFAULT)) : '';
 
-    $sql = "INSERT INTO usuarios (nome, email, data_nascimento, senha) VALUES ('$nome', '$email', '$data_nascimento', '$senha')";
+        $sql = "INSERT INTO usuarios (nome, email, data_nascimento, senha) VALUES ('$nome', '$email', '$data_nascimento', '$senha')";
 
-    mysqli_query($conexao, $sql);
+        mysqli_query($conexao, $sql);
 
-    //verificacao de criacao do usuario
-    if(mysqli_affected_rows($conexao) > 0){
-        $_SESSION['mensagem'] = 'Usuário criado com sucesso.';
-        header('Location: index.php');
-        exit;
-    }else{
-        $_SESSION['mensagem'] = 'Usuário não foi criado!';
-        header('Location: index.php');
-        exit;
+        //verificacao de criacao do usuario
+        if(mysqli_affected_rows($conexao) > 0){
+            $_SESSION['mensagem'] = 'Usuário criado com sucesso.';
+            header('Location: index.php');
+            exit;
+        }else{
+            $_SESSION['mensagem'] = 'Usuário não foi criado!';
+            header('Location: index.php');
+            exit;
+        }
     }
-}
-
 ?>
--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar bg-body-tertiary">
+<nav class="navbar bg-dark bg-body-tertiary">
   <div class="container">
     <a class="navbar-brand" href="#">
       <img src="./assets/img/acto.svg" alt="acto logo" width="100" height="auto">
